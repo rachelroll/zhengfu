f42fa356b1e04848d397de7400d8a4c787552a8f
rachelroll <hai30@163.com>
2019-09-19 16:37:50 +0800

    init

---
+++ resources/js/routes.js
@@ -0,0 +1,30 @@
+import Welcome from './pages/Welcome.vue';
+import Login from './pages/Login.vue';
+import Apply from './pages/Apply.vue';
+import Employers from './pages/Employers.vue';
+
+export default {
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            component: Welcome,
+            name: 'welocome'
+        },
+        {
+            path: '/login',
+            component: Login,
+            name: 'login'
+        },
+        {
+            path: '/apply',
+            component: Apply,
+            name: 'apply'
+        },
+        {
+            path: '/employers',
+            component: Employers,
+            name: 'employers'
+        }
+    ]
+};
